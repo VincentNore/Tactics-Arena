c6090bfe4d0d3469fe6dfb6111d9923fa0c2f4d7
Eldariur <35937370+Eldariur@users.noreply.github.com>
2018-02-06 15:01:35 +0100

    Fonction choix joueur

---
+++ ChoixJoueur.c
@@ -0,0 +1,12 @@
+/* EL KANDOUSSI ADNAN */
+/* Fonctions de choix du joueur par tour */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Fonction de sélection d'unité par tour */
+
+void deplacement(int tour){
+
+
