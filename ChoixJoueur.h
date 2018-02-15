f785da42b2257f0c394cf4c0f70bed2887891236
Eldariur <35937370+Eldariur@users.noreply.github.com>
2018-02-15 10:22:44 +0100

    Choixjoueur header

---
+++ ChoixJoueur.h
@@ -0,0 +1,10 @@
+#ifndef CHOIX_JOUEUR
+#define CHOIX_JOUEUR
+
+
+
+void ChoixDeplacement(int tour); /*Fonction de selection unité /tour */
+
+void DebutPartie(); /*Fonction de lancement Partie */
+
+#endif
