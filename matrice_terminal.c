1adbea3d85298b64dbd91ddab4e3985ecf10b84b
VincentNore <35962254+VincentNore@users.noreply.github.com>
2018-02-15 10:42:16 +0100

    Création de la fonction pour placer les décors

--- matrice_terminal.c
+++ matrice_terminal.c
@@ -3,8 +3,15 @@
 #include<math.h>
 
 #define N 11
-#define MAX 2
+
+#define MAX 11
 #define MIN 0
+
+#define MAX_pierre 3
+#define MIN_pierre 0
+
+#define MAX_eau 3
+#define MIN_eau 0
 
 char matrice[N][N];
 char joueur1 = 'o';
@@ -23,15 +30,33 @@ void init_matrice(){
 	}	
 }
 
-/*void placer_decors(){
-	int i, j;
+void placer_decors(){
+	int i, j, nb_ligne, nb_colonne ,nb_pierre, qts_eau;
 	srand(time(NULL));
-    nb_objet = (rand() % (MAX - MIN + 1)) + MIN;
-    for(i=0; i<nb; i++){
-    	for(j=0; j<nb
-			matrice[nb][nb] = eau;
+	nb_pierre = (rand() % (MAX_pierre - MIN_pierre + 1)) + MIN;
+	for(i=0; i<nb_pierre; i++){
+		nb_colonne = (rand() % (MAX - MIN + 1)) + MIN;
+		nb_ligne = (rand() % (MAX - MIN + 1)) + MIN;
+		matrice[nb_ligne][nb_colonne] = pierre;
 	}
-}*/
+	
+	qts_eau = (rand() % (MAX_eau - MIN_eau + 1)) + MIN;
+	for(j=0; j<qts_eau; j++){
+		nb_colonne = (rand() % (MAX - MIN + 1)) + MIN;
+		nb_ligne = (rand() % (MAX - MIN + 1)) + MIN;
+		matrice[nb_ligne][nb_colonne] = eau;
+		matrice[nb_ligne+1][nb_colonne] = eau;
+		matrice[nb_ligne][nb_colonne+1] = eau;
+		matrice[nb_ligne+1][nb_colonne+1] = eau;
+		matrice[nb_ligne-1][nb_colonne] = eau;
+		matrice[nb_ligne][nb_colonne-1] = eau;
+		matrice[nb_ligne-1][nb_colonne-1] = eau;
+		matrice[nb_ligne+1][nb_colonne-1] = eau;
+		matrice[nb_ligne-1][nb_colonne+1] = eau;
+		
+	}
+}
+
 
 void afficher_matrice(){
 	int i, j;
