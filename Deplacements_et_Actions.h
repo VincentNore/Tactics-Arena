7539c5a585233b4294240a2a554ae02329864640
cjolliet <corentin.jolliet.etu@univ-lemans.fr>
2018-02-20 16:34:54 +0100

    Add files via upload
    Ajout des fonctions de combats (A corriger car buguée); fonction de déplacement à compléter ; fonction de comptage de cases à tester/corriger.

--- Deplacements_et_Actions.h
+++ Deplacements_et_Actions.h
@@ -33,6 +33,70 @@ int Compte_Cases(t_sbires sbires, char mat[][], int direction){ /*Fonction de co
 	return nbr_cases;
 }
 
+t_sbires Combat_physique(t_sbires sbires1, t_sbires sbires2, int attaquant){
+/*Fonction calculant les dégâts physiques infligés avec une attaque normale*/
+//A CORRIGER
+	int degats;
+	switch(attaquant){
+		case 1 : printf("%s attaque %s\n", sbires1.nom, sbires2.nom);
+			degats = (sbires1.attaque - sbires2.defense)/10 + (sbires1.attaque - sbires2.defense)%10;
+			if( (rand()%(2*sbires2.esquive) ) < (sbires2.esquive + degats) ){ 
+				sbires2.vie = sbires2.vie - degats;
+				printf("%s reçoit %i dégats\n", sbires2.nom, degats);
+				printf("Il reste %i PVs à %s\n", sbires2.vie, sbires2.nom);
+				return sbires2;
+			} else printf("%s a esquivé !\n", sbires2.nom);
+			break;
+		case 2 : printf("%s attaque %s\n", sbires2.nom, sbires1.nom);
+			degats = (sbires2.attaque - sbires1.defense)/10 + (sbires2.attaque - sbires1.defense)%10;
+			if( (rand()%(2*sbires1.esquive) ) < (sbires1.esquive + degats) ){ 
+				sbires1.vie = sbires1.vie - degats;
+				printf("%s reçoit %i dégats\n", sbires1.nom, degats);
+				return sbires1;
+			} else printf("%s a esquivé !\n", sbires1.nom);
+			break;
+		default : printf("Erreur d'attaquant\n");
+			break;
+	}
+	if( attaquant == 1){
+		return sbires2;
+	} else {
+		return sbires1;
+	}
+}
+
+t_sbires Combat_magique(t_sbires sbires1, t_sbires sbires2, int attaquant){
+/*Fonction calculant les dégâts magiques infligés avec une attaque magique*/
+//A CORRIGER
+	int degats;
+	switch(attaquant){
+		case 1 : printf("%s attaque %s\n", sbires1.nom, sbires2.nom);
+			degats = (( sbires1.attaque*(sbires1.mana/10) ) - sbires2.defense )/10 + (( sbires1.attaque*(sbires1.mana/10) ) - sbires2.defense )%10;
+			if( (rand()%(2*sbires2.esquive)) < (sbires2.esquive + degats) ){ 
+				sbires2.vie = sbires2.vie - degats;
+				printf("%s reçoit %i dégats magiques\n", sbires2.nom, degats);
+				return sbires2;
+			} else printf("%s a esquivé !\n", sbires2.nom);
+			break;
+		case 2 : printf("%s attaque %s\n", sbires2.nom, sbires1.nom);
+			degats = ( sbires2.attaque*(sbires2.mana/10) - sbires1.defense )/10 + ( sbires2.attaque*(sbires2.mana/10) - sbires1.defense )%10;
+			if( (rand()%(2*sbires1.esquive)) < (sbires1.esquive + degats) ){ 
+				sbires1.vie = sbires1.vie - degats;
+				printf("%s reçoit %i dégats magiques\n", sbires1.nom, degats);
+				return sbires1;
+			} else printf("%s a esquivé !\n", sbires1.nom);
+			break;
+		default : printf("Erreur d'attaquant\n");
+			break;
+	}
+	if(attaquant == 1){
+		return sbires2;
+	} else {
+		return sbires1;
+	}
+}
+
+
 void Deplacement(t_sbires sbires, char mat[][]){ /*Fonction de déplacement de l'unité : /!\ A COMPLETER /!\ */
 	int choix = 0;
 	int nbr_cases = 0;
@@ -42,7 +106,7 @@ void Deplacement(t_sbires sbires, char mat[][]){ /*Fonction de déplacement de l
 	printf("Déplacements vertical ou horizontal ?\n");
 	printf("1 : Vertical\n2 : Horizontal\n3 : Annuler Déplacement\nChoix : ");
 	
-	scanf("%i", choix);
+	scanf("%i", &choix);
 	
 	switch(choix){
 		case 1 : printf("Déplacement vertical\n");
