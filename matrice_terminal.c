2c188204f45f0e715b51b317f830bf3d23786718
VincentNore <35962254+VincentNore@users.noreply.github.com>
2018-02-20 14:20:22 +0100

    Add structure pour decors et joueurs
    Utilisation des structures pour les éléments de décoration et les joueurs

--- matrice_terminal.c
+++ matrice_terminal.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<time.h>
+#include"matrice_terminal.h"
 
 #define N 11
 
@@ -14,14 +16,11 @@
 #define MIN_eau 0
 
 char matrice[N][N];
-char joueur1 = 'o';
-char joueur2 = 'x';
-char libre = '.';
-char eau = 'e';
-char pierre = 'p';
+
 
 /*Fonction pour initialiser la matrice à vide*/
 void init_matrice(){
+	char libre = '.';
 	int i, j;
 	for (i=0; i<N; i++) {
 		for (j=0; j<N; j++) {
@@ -35,27 +34,35 @@ void init_matrice(){
 /*Fonction pour placer les décors*/
 void placer_decors(){
 	int i, j, nb_ligne, nb_colonne ,nb_pierre, qts_eau;
+	
+	decors_t decors;
+	
+	decors.eau = 'e';
+	decors.pierre = 'p';
+	
 	srand(time(NULL));
 	nb_pierre = (rand() % (MAX_pierre - MIN_pierre + 1)) + MIN; /*Génération d'un nombre aléatoire pour les pierres*/
+	
 	for(i=0; i<nb_pierre; i++){
 		nb_colonne = (rand() % (MAX - MIN + 1)) + MIN; /*Génération d'un nombre aléatoire pour les colonne*/
 		nb_ligne = (rand() % (MAX - MIN + 1)) + MIN; /*Génération d'un nombre aléatoire pour les lignes*/
-		matrice[nb_ligne][nb_colonne] = pierre;
+		matrice[nb_ligne][nb_colonne] = decors.pierre;
 	}
 	
 	qts_eau = (rand() % (MAX_eau - MIN_eau + 1)) + MIN; /*Génération d'un nombre aléatoire pour l'eau*/
+	
 	for(j=0; j<qts_eau; j++){
 		nb_colonne = (rand() % (MAX - MIN + 1)) + MIN;
 		nb_ligne = (rand() % (MAX - MIN + 1)) + MIN;
-		matrice[nb_ligne][nb_colonne] = eau;
-		matrice[nb_ligne+1][nb_colonne] = eau;
-		matrice[nb_ligne][nb_colonne+1] = eau;
-		matrice[nb_ligne+1][nb_colonne+1] = eau;
-		matrice[nb_ligne-1][nb_colonne] = eau;
-		matrice[nb_ligne][nb_colonne-1] = eau;
-		matrice[nb_ligne-1][nb_colonne-1] = eau;
-		matrice[nb_ligne+1][nb_colonne-1] = eau;
-		matrice[nb_ligne-1][nb_colonne+1] = eau;
+		matrice[nb_ligne][nb_colonne] = decors.eau;
+		matrice[nb_ligne+1][nb_colonne] = decors.eau;
+		matrice[nb_ligne][nb_colonne+1] = decors.eau;
+		matrice[nb_ligne+1][nb_colonne+1] = decors.eau;
+		matrice[nb_ligne-1][nb_colonne] = decors.eau;
+		matrice[nb_ligne][nb_colonne-1] = decors.eau;
+		matrice[nb_ligne-1][nb_colonne-1] = decors.eau;
+		matrice[nb_ligne+1][nb_colonne-1] = decors.eau;
+		matrice[nb_ligne-1][nb_colonne+1] = decors.eau;
 		
 	}
 }
@@ -63,17 +70,23 @@ void placer_decors(){
 
 /*Fonction pour placer les pions des deux joueurs*/
 void placer_pions(){
+	
+	joueurs_t joueurs;
+	
+	joueurs.joueur1 = 'o';
+	joueurs.joueur2 = 'x';
+
 	int i, j;
 	for(i=0; i<1; i++){
 		for(j=0; j<N; j++){
-			matrice[i][j] = joueur1;
+			matrice[i][j] = joueurs.joueur1;
 		}
 	}
 	
 	for(i=0; i<N; i++){
 		for(j=0; j<N; j++){
 			if(i == 10){
-				matrice[i][j] = joueur2;
+				matrice[i][j] = joueurs.joueur2;
 			}
 		}
 	}	
