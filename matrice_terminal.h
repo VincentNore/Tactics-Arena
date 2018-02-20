5044455ed095123d40d1977fe50ed3cb8802ce9e
VincentNore <35962254+VincentNore@users.noreply.github.com>
2018-02-20 14:21:16 +0100

    Ajout des structures des decors et des joueurs

--- matrice_terminal.h
+++ matrice_terminal.h
@@ -2,6 +2,16 @@
 #include<stdlib.h>
 #include<math.h>
 
+typedef struct decors_s{
+		char eau; 
+		char pierre;
+	} decors_t;
+	
+typedef struct joueurs_s{
+		char joueur1; 
+		char joueur2;
+	} joueurs_t;
+
 void init_matrice();
 void placer_decors();
 void placer_pions();
