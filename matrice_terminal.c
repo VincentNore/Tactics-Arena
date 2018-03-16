2d6c5c37a7e6d2962efa13fe3944eb54cb6e8751
VincentNore <35962254+VincentNore@users.noreply.github.com>
2018-03-16 16:32:55 +0100

    Add files via upload

--- matrice_terminal.c
+++ matrice_terminal.c
@@ -6,8 +6,11 @@
 
 #define N 11
 
-#define MAX 11
+#define MAX 10
 #define MIN 0
+
+#define MAX_pion 10
+#define MIN_pion 0
 
 #define MAX_pierre 3
 #define MIN_pierre 0
@@ -31,7 +34,7 @@ void init_matrice(char matrice_affichage[N][N], int matrice_indice[N][N]){
 
 /*Fonction pour placer les décors*/
 void placer_decors(char matrice_affichage[N][N], int matrice_indice[N][N], decors_t decors){
-	int i, j, nb_ligne, nb_colonne ,nb_pierre, qts_eau;
+	int i, j, nb_ligne=0, nb_colonne=0, nb_pierre=0, qts_eau=0;
 	
 	srand(time(NULL));
 	nb_pierre = (rand() % (MAX_pierre - MIN_pierre + 1)) + MIN; /*Génération d'un nombre aléatoire pour les pierres*/
@@ -50,61 +53,54 @@ void placer_decors(char matrice_affichage[N][N], int matrice_indice[N][N], decor
 		nb_ligne = (rand() % (MAX - MIN + 1)) + MIN;
 		matrice_affichage[nb_ligne][nb_colonne] = decors.eau;
 		matrice_indice[nb_ligne][nb_colonne] = 3;
-		
+
 		matrice_affichage[nb_ligne+1][nb_colonne] = decors.eau;
 		matrice_indice[nb_ligne+1][nb_colonne] = 3;
-		
+
 		matrice_affichage[nb_ligne][nb_colonne+1] = decors.eau;
 		matrice_indice[nb_ligne][nb_colonne+1] = 3;
-		
+
 		matrice_affichage[nb_ligne+1][nb_colonne+1] = decors.eau;
 		matrice_indice[nb_ligne+1][nb_colonne+1] = 3;
 		
 		matrice_affichage[nb_ligne-1][nb_colonne] = decors.eau;
 		matrice_indice[nb_ligne-1][nb_colonne] = 3;
-		
+
 		matrice_affichage[nb_ligne][nb_colonne-1] = decors.eau;
 		matrice_indice[nb_ligne][nb_colonne-1] = 3;
-		
+
 		matrice_affichage[nb_ligne-1][nb_colonne-1] = decors.eau;
 		matrice_indice[nb_ligne-1][nb_colonne-1] = 3;
-		     
+
 		matrice_affichage[nb_ligne+1][nb_colonne-1] = decors.eau;
 		matrice_indice[nb_ligne+1][nb_colonne-1] = 3;
-		
+
 		matrice_affichage[nb_ligne-1][nb_colonne+1] = decors.eau;
 		matrice_indice[nb_ligne-1][nb_colonne+1] = 3;
-		
 	}
 }
 
 /*Fonction pour placer les pions des deux joueurs*/
-void placer_pions(char matrice_affichage[N][N], int matrice_indice[N][N], sbires sbire_bleu, sbires sbire_rouge){
+sbires placer_pions(char matrice_affichage[N][N], int matrice_indice[N][N], sbires sbire_bleu, sbires sbire_rouge){
 
-	int i, j;
-	for(i=0; i<1; i++){
-		for(j=0; j<2; j++){
-			matrice_affichage[i][j] = sbire_bleu.symbole;
-			matrice_indice[i][j] = 1;
-			sbire_bleu.position[0] = i;
-			sbire_bleu.position[1] = j;
-			printf("\n%i\n", sbire_bleu.position[0]);
-			printf("%i\n\n", sbire_bleu.position[1]);
-		}
-	}
+	const int a = 0, i = 10;
+	int b = 0, j = 0;
 	
-	for(i=0; i<N; i++){
-		for(j=0; j<2; j++){
-			if(i == 10){
-				matrice_affichage[i][j] = sbire_rouge.symbole;
-				matrice_indice[i][j] = 1;
-				sbire_rouge.position[0] = i;
-				sbire_rouge.position[1] = j;
-				printf("\n%i\n", sbire_rouge.position[0]);
-				printf("%i\n\n", sbire_rouge.position[1]);
-			}
-		}
-	}	
+	b = (rand() % (MAX_pion - MIN_pion + 1)) + MIN;
+			
+	matrice_affichage[a][b] = sbire_bleu.symbole;
+	matrice_indice[a][b] = 1;
+	sbire_bleu.position[0] = a;
+	sbire_bleu.position[1] = b;
+
+	j = (rand() % (MAX_pion - MIN_pion + 1)) + MIN;
+
+	matrice_affichage[i][j] = sbire_rouge.symbole;
+	matrice_indice[i][j] = 1;
+	sbire_rouge.position[0] = i;
+	sbire_rouge.position[1] = j;
+	
+	return(sbire_rouge);
 }
 
 /*Fonction afficher la matrice*/
