625a11238e274c562593ede4e536960662937078
VincentNore <35962254+VincentNore@users.noreply.github.com>
2018-04-04 19:01:33 +0200

    Add files via upload

---
+++ src/matrice_terminal.c
@@ -0,0 +1,198 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+#include<string.h>
+#include"matrice_terminal.h"
+#include"couleur.h"
+#include"structures.h"
+
+#define N 11
+
+
+/*Fonction d'initialisation de la matrice à vide*/
+void init_matrice(int mat[N][N]){
+	int i, j;
+	for (i=0; i<N; i++) {
+		for (j=0; j<N; j++) {
+			mat[i][j] = 0;	
+		}
+	}	
+}
+
+/*Fonction d'affichage de la matrice*/
+int afficher_matrice(int mat[N][N], armee_t armee_j1, armee_t armee_j2){
+
+	clrscr();
+	for(int i = 0; i < N; i++){
+		
+		printf("\n");
+		for(int j = 0; j < N; j++){
+			printf("| ");
+			
+			if((i == armee_j1.A.position[0]) && (j == armee_j1.A.position[1]) && armee_j1.A.vie > 0){
+				couleur("31");
+				printf("%s", armee_j1.A.symbole);
+				couleur("0");
+			} 
+			else if((i == armee_j1.B.position[0]) && (j == armee_j1.B.position[1]) && armee_j1.B.vie > 0){
+				couleur("31");
+				printf("%s", armee_j1.B.symbole);
+				couleur("0");
+			}
+			else if((i == armee_j1.C.position[0]) && (j == armee_j1.C.position[1]) && armee_j1.C.vie > 0){
+				couleur("31");
+				printf("%s", armee_j1.C.symbole);
+				couleur("0");
+			}
+			else if((i == armee_j1.D.position[0]) && (j == armee_j1.D.position[1]) && armee_j1.D.vie > 0){
+				couleur("31");
+				printf("%s", armee_j1.D.symbole);
+				couleur("0");
+			}
+			else if((i == armee_j1.E.position[0]) && (j == armee_j1.E.position[1]) && armee_j1.E.vie > 0){
+				couleur("31");
+				printf("%s", armee_j1.E.symbole);
+				couleur("0");
+			}
+			else {
+				printf("  ");
+				mat[i][j] = 0;
+			}
+			
+			 if((i == armee_j2.A.position[0]) && (j == armee_j2.A.position[1]) && armee_j2.A.vie > 0){
+			 	couleur("32");
+				printf("%s", armee_j2.A.symbole);
+				couleur("0");
+			}
+			else if((i == armee_j2.B.position[0]) && (j == armee_j2.B.position[1]) && armee_j2.B.vie > 0){
+				couleur("32");
+				printf("%s", armee_j2.B.symbole);
+				couleur("0");
+			}
+			else if((i == armee_j2.C.position[0]) && (j == armee_j2.C.position[1]) && armee_j2.C.vie > 0){
+				couleur("32");
+				printf("%s", armee_j2.C.symbole);
+				couleur("0");
+			}
+			else if((i == armee_j2.D.position[0]) && (j == armee_j2.D.position[1]) && armee_j2.D.vie > 0){
+				couleur("32");
+				printf("%s", armee_j2.D.symbole);
+				couleur("0");
+			}
+			else if((i == armee_j2.E.position[0]) && (j == armee_j2.E.position[1]) && armee_j2.E.vie > 0){
+				couleur("32");
+				printf("%s", armee_j2.E.symbole);
+				couleur("0");
+			}
+			else {
+				printf(" ");
+				mat[i][j] = 0;
+			}
+		}
+		printf(" |");
+	}
+	printf("\n\n");
+}
+
+sbires_t positionner_sbire(sbires_t sbire, int mat[N][N]){
+/*Fonction de verification de position d'unite*/
+	for(int n = 0; n < 2; n++){
+		if(n == 0){
+			printf("Ligne : ");
+		} 
+		else {
+			printf("Colonne : ");
+		}
+	
+		scanf("%i", &sbire.position[n]);
+		sbire.position[n] = sbire.position[n] - 1;
+		while((sbire.position[n] < 0) || (sbire.position[n] >= N)){
+
+			printf("\nLigne/Colonne visée n'existe pas, veuillez choisir un numéro de Ligne/Colonne entre 1 et 11 : ");
+			scanf("%i", &sbire.position[n]);
+			sbire.position[n] = sbire.position[n] - 1;
+		}
+	}
+
+	if(mat[sbire.position[0]][sbire.position[1]] == 0){
+			mat[sbire.position[0]][sbire.position[1]] = 1;
+			return(sbire);
+
+	} 
+	else if(mat[sbire.position[0]][sbire.position[1]] != 0) {
+
+		printf("La case %i - %i est déjà occupée\n", (sbire.position[0] + 1), (sbire.position[1] + 1));
+		sbire = positionner_sbire(sbire, mat);
+		return sbire;
+	}
+}
+
+armee_t init_armee_j1(armee_t armee_j1, sbires_t sbire, int mat[N][N]){
+
+	int j;	
+	
+	clrscr();
+	
+	j = 1;
+	printf("--- Joueur 1 - unité 1 ---\n");
+	armee_j1.A = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 2 ---\n");
+	armee_j1.B = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 3 ---\n");
+	armee_j1.C = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 4 ---\n");
+	armee_j1.D = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 5 ---\n");
+	armee_j1.E = init_sbire(sbire, mat);
+	
+	return armee_j1;
+}
+
+armee_t init_armee_j2(armee_t armee_j2, sbires_t sbire, int mat[N][N]){
+
+	int j;	
+	
+	clrscr();
+	
+	j = 1;
+	printf("--- Joueur 1 - unité 1 ---\n");
+	armee_j2.A = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 2 ---\n");
+	armee_j2.B = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 3 ---\n");
+	armee_j2.C = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 4 ---\n");
+	armee_j2.D = init_sbire(sbire, mat);
+	printf("--- Joueur 1 - unité 5 ---\n");
+	armee_j2.E = init_sbire(sbire, mat);
+	
+	return armee_j2;
+}
+
+sbires_t init_sbire(sbires_t sbire, int mat[N][N]){
+/*Fonction d'initialisation d'unites*/
+	sbire.nom = malloc(sizeof(char)*128);
+	sbire.symbole = malloc(sizeof(char)*128);
+	printf("\nNom de l'unité : ");
+	scanf("%s", sbire.nom);
+	
+	printf("\nSymbole de l'unité : ");
+	scanf("%s", sbire.symbole);
+	
+	sbire.attaque = 30;
+	sbire.defense = 20;
+	sbire.vie = 50;
+	sbire.mana = 20;
+	sbire.esquive = 30;
+	sbire.nbr_depl = 5;
+	printf("Position de départ : \n");
+		
+	sbire = positionner_sbire(sbire, mat);
+	mat[sbire.position[0]][sbire.position[1]] = 1;
+
+	printf("\nSbire %s créé : \n", sbire.nom);
+	printf("Attaque : %i \nDefense : %i \nMana : %i \nVie : %i \nTaux d'esquive : %i pourcents\nNombre de pas possibles : %i \n", sbire.attaque, sbire.defense, sbire.mana, sbire.vie, sbire.esquive, sbire.nbr_depl);
+	return(sbire);
+}
+
